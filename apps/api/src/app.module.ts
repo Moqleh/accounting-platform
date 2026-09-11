d4b4fc41a18b944d51cb0e5534e96f5988efa4fc
Moqleh <moh.oglah@gmail.com>
2026-09-11 20:08:56 +0300

    feat(api): register accounting and ERP modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { AccountingController } from './accounting.controller';
+import { ErpController } from './erp.controller';
+import { AccountingService } from './accounting.service';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  controllers: [AppController],
-  providers: [PrismaService],
+  controllers: [AppController, AccountingController, ErpController],
+  providers: [PrismaService, AccountingService],
 })
 export class AppModule {}
