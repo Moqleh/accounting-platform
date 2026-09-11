088af731082e7e29b91e535984b8a9f54c1eec16
Moqleh <moh.oglah@gmail.com>
2026-09-11 20:17:24 +0300

    feat(api): register payments module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,13 +4,15 @@ import { AccountingController } from './accounting.controller';
 import { ErpController } from './erp.controller';
 import { SalesController } from './sales.controller';
 import { PurchasesController } from './purchases.controller';
+import { PaymentsController } from './payments.controller';
 import { AccountingService } from './accounting.service';
 import { SalesService } from './sales.service';
 import { PurchasesService } from './purchases.service';
+import { PaymentsService } from './payments.service';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  controllers: [AppController, AccountingController, ErpController, SalesController, PurchasesController],
-  providers: [PrismaService, AccountingService, SalesService, PurchasesService],
+  controllers: [AppController, AccountingController, ErpController, SalesController, PurchasesController, PaymentsController],
+  providers: [PrismaService, AccountingService, SalesService, PurchasesService, PaymentsService],
 })
 export class AppModule {}
